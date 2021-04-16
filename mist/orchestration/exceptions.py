59f3f3872bf8332a66db9d637ed552608fe21e16
Chris Karageorgiou Kaneen <csk@mist.io>
2021-04-16 12:27:59 +0000

    Remove docker_run() definition from methods

--- mist/orchestration/exceptions.py
+++ mist/orchestration/exceptions.py
@@ -1,10 +1,5 @@
 from mist.api.exceptions import BadRequestError
-from mist.api.exceptions import ServiceUnavailableError
 
 
 class TemplateParseError(BadRequestError):
     msg = "Failed to parse template"
-
-
-class WorkflowExecutionError(ServiceUnavailableError):
-    msg = "Failed to execute workflow"
